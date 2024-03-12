fe017626336f0794581468f1827a50fabeaf7a49
SJBM1876 <137792828+SJBM1876@users.noreply.github.com>
2024-03-12 13:35:18 +0000

    Add files via upload

---
+++ blackjack.js
@@ -0,0 +1,66 @@
+
+let cards = [];
+let sum = 0;
+let blackJack = false;
+let isAlive = false;
+let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+
+function getRandomCard() {
+  let randomNumber = Math.floor(Math.random() * 13) + 1;
+  if (randomNumber > 10) {
+  return 10;
+  } else if (randomNumber === 1){
+  return 11;
+  } else {
+  return randomNumber;
+  }
+}
+
+
+
+function startGame() {
+	isAlive = true;
+  let firstCard = getRandomCard();
+	let secondCard = getRandomCard();
+	cards = [firstCard, secondCard];
+	sum = firstCard + secondCard;
+   
+  renderGame();
+}
+
+
+
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+   
+   for (let i = 0; i < cards.length; i++) {
+   		cardsEl.textContent += cards[i] + " ";
+   }
+   
+   sumEl.textContent = "Sum: " + sum;
+    if (sum <= 20) {
+        message = "Do you want to pick another card?"
+    } else if (sum === 21) {
+        message = "Congratulations!"
+        blackJack = true;
+    } else {
+        message = "Sorry, you've lost!"
+        isAlive = false;
+    }
+
+
+    messageEl.textContent = message;
+}
+
+function newCard() {
+
+		if (isAlive === true && blackJack === false) {
+   	 	let card = getRandomCard();
+    	sum += card;
+    	cards.push(card);
+    	renderGame(); 
+    }
+}
